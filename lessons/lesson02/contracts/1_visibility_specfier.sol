0b1ebfde66132ab5eae6ccc7ffcb1cafa8597293
Saurav <49294257+sauravyadav19@users.noreply.github.com>
2025-06-29 11:38:08 +0530

    L2: Fix some typos

--- lessons/lesson02/contracts/1_visibility_specfier.sol
+++ lessons/lesson02/contracts/1_visibility_specfier.sol
@@ -50,7 +50,7 @@ contract ParentHouse{
     under the hood the data arguments passed by that external user's call of the functions are stored
     in a cheap read-only place, that place is call calldata, ones the values are read and are of no more 
     of use calldata is discarded, keeping it gas efficient; where as when the data is to be held for the 
-    duration of the transactions, it is stored in memory, as this complex and requires allocation and deallocation
+    duration of the execution of the function, it is stored in memory, as this complex and requires allocation and deallocation
     of the space, storing in it would be expensive (higher gas fee),think of calldata as CPU registers and Memory as RAM.
 
 
@@ -58,11 +58,11 @@ contract ParentHouse{
     is only meant to be called from the outside of the contract and hence you can store the data arugmnet that comes
     with the call of that resource(fucntion) in calldata, i assure you that the data will not be needed within the contract
     and so you can store in that tempoary location call calldata.
-    but when we are marking a resouce(function) with public visibility specifer we are being ambiogous in the sense of where 
+    but when we are marking a resouce(function) with public visibility specifer we are being ambiguous in the sense of where 
     this function can be called from,compiler assumes the complex and the worst case which is that it might be called from
-    within the contract and hence it is required to keep that temporary data location into more "permanent" data location
-    and hence copies it to memory and as copying of the data requires space with is expensive and hence increasing the gas fee
-    and making it costly, this may seems like a small thing at first but if you imagine that function called multiple times 
+    within the contract and hence it is required to keep that temporary location's data into more "permanent" data location
+    and hence copies it to the memory and as copying of the data requires space which is expensive and hence increasing the gas fee
+    and making it costly, this may seems like a small thing at first but if you imagine that function being called multiple times 
     that cumulates to a large overhead cost.
 
     so it is alway the best practice to mark the resouce(functions) external which you think are requied only for the tempoary
