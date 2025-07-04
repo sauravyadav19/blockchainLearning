c7c8ca8c66fd640abdec1e374261e5f1b80b8343
Saurav <sauravsaurav0@gmail.com>
2025-07-04 12:23:38 +0000

    L3:Add detail comments about dyanmic arrays and initialization of them with non-fixed size members strcutrues

--- lessons/lesson03/contracts/2_Structure.sol
+++ lessons/lesson03/contracts/2_Structure.sol
@@ -18,6 +18,26 @@ contract LearnStructures{
     // creating an array of funders
     funderDetail [] public funders;
 
+    //we CANNOT initialize the above array something like this
+    // funderDetail [] public funders = [{name:"Jon", amount:10},{name:"Araya", amount:20}]
+    //WHY? but before we can answer that it is important that we know that something like this would be acceptable
+    // uint[] anArrayofFavNumbers = [1,2,3,4,5,6,7,8] OR bool [] an arrayofBooValues = [true,false,false]
+    // this is becasue the compiler know what size of values these are and by that providing an fix or known
+    // amount of gas requried
+    // but when we are creating our own type using struct compiler see a variable type as in it does not have a fix size
+    // there is string in funderDetail structure which could vary frome element to element of funderDetail type 
+    // this variablilty prevents compiler to use one-fit-all type of work for each element to it by desing enforce
+    // seperation of  the creation and initialization in differnt steps and hence allowing compiler to know the gas cost
+    // in technical terms, you cannot create a state varaible of type with non-fixed size members and keep it dynamic and 
+    // also initialize it all at the same time.
+    // in terms of analogy, you are asking the builder(EVM) to build a bookself of whose capcity you do not know and hence
+    // want it to extenable (dynamic), but also asking them at the same time to fit some certain books,
+    //  this is not acceptable desing.
+    /// in terms of a more technical analogy, you are asking database to create a table and also insert the value at 
+    // the same time, basically doing this CREATE TABLE(Name char(10), Amount Integer) and ROWS (("Saurav",10), ("Dancerys",20))
+    //  these two are different steps first you create the table then insert the values and not do it together
+
+
     constructor(){
         funderDetail memory funder;
         funder.name = "Jon";
